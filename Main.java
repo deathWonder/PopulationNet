e0764d9d3c42645028b22dfc5a928e1bbd9244bf
Sabir <bovba179@gmail.com>
2023-02-15 00:56:16 +0300

    Initial Population

---
+++ Main.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+
+        // Найти количество несовершеннолетних (т.е. людей младше 18 лет).
+        long count = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + count);
+
+        //Получить список фамилий призывников (т.е. мужчин от 18 и до 27 лет).
+
+        List<String> recruiters = persons.stream()
+                .filter(person -> person.getAge() >= 18)
+                .filter(person -> person.getAge() < 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+
+        System.out.println(recruiters);
+
+        //Получить отсортированный по фамилии список потенциально работоспособных людей с высшим образованием
+        // в выборке (т.е. людей с высшим образованием от 18 до 60 лет для женщин и до 65 лет для мужчин).
+
+        List<Person> laborClass = persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER)
+                .filter(person -> {
+                    boolean result;
+                    if (person.getSex() == Sex.MAN) {
+                        result = person.getAge() < 60;
+                    } else {
+                        result = person.getAge() < 65;
+                    }
+                    return result;
+                })
+                .filter(person -> person.getAge() >= 18)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+
+        System.out.println(laborClass);
+
+    }
+}
